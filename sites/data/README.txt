0a3d37f6cda12207b0caf30ea57d83941ceeefe4
bourgault <bourgault@users.noreply.github.com>
2022-12-21 07:36:41 -0500

    modif 21/12/2023

---
+++ sites/data/README.txt
@@ -0,0 +1,3 @@
+https://coursera-jhu-default-rtdb.firebaseio.com/categories.json
+
+https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/{name}.json
--- sites/js/script.js
+++ sites/js/script.js
@@ -7,37 +7,108 @@ $(function () {
 			$("#collapdable-nav").collapse('hide');
 		}
 	});
+
+// In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+
+
 });
 
 (function (global) {
 
 var dc = {};
-var homeHtml = "../snippets/home-snippet.html";
+var homeHtml = "./snippets/home-snippet.html";
+var allCategoriesURL = "/data/categories.json";
+var categoriesTitleHtml = "snippets/menu-title-snippet.html";
+var categoryHtml = "snippets/menu-snippet.html";
 
 // function to insert html
 var insertHtml = function (selector, html) {
 	var targetElem = document.querySelector(selector);
-	targetElem.insertHtml = html;
+	targetElem.innerHTML = html;
 };
 
 // function to show icon while loding page
-var showLoadind = function (selector) {
+var showLoading = function (selector) {
 	var html = "<div class='text-center'>";
-	html += "<img src='../images/ajax-loader.gif'></div>";
+	html += "<img src='./images/ajax-loader.gif'></div>";
 	insertHtml(selector, html);
 };
+
+// Substitute of property {{name}}, etc....
+var insertProperty = function (string, propName, propValue) {
+	var propToReplace = "{{" + propName + "}}";
+	string = string.replace(new RegExp(propToReplace, "g"), propValue);
+	return string;
+}
+
+
 
 // On page load
 document.addEventListener("DOMContentLoaded", function (event) {
 
-	showLoading("main-content");
+	showLoading("#main-content");
 	$ajaxUtils.sendGetRequest(
 		homeHtml,
 		function (responseText) {
-			document.querySelector("main-content").innerHTML = responseText;
+			document.querySelector("#main-content").innerHTML = responseText;
 		},
 		false);
 });
+
+// Load menu categories
+dc.loadMenuCategories = function () {
+	showLoading("#main-content");
+	$ajaxUtils.sendGetRequest(allCategoriesURL, buildAndShowCategoriesHtml);
+}
+
+// Build Categories HTML  from data
+function buildAndShowCategoriesHtml (categories) {
+	// load categories
+	$ajaxUtils.sendGetRequest(
+		categoriesTitleHtml,
+		function (categoriesTitleHtml) {
+			//retrieve single category snippet
+			$ajaxUtils.sendGetRequest(
+				categoryHtml,
+				function(categoryHtml) {
+					var categoryViewHtml = 
+						buildCategoriesViewHtml(categories,
+														categoriesTitleHtml,
+														categoryHtml);
+					insertHtml("#main-content", categoryViewHtml);
+				}, 
+				false);	
+		}, 
+		false);
+}
+
+// Build categories view
+function buildCategoriesViewHtml (categories,
+											 categoriesTitleHtml,
+											 categoryHtml) {
+
+	var finalHtml = categoriesTitleHtml;
+	finalHtml += "<section class='row'>";
+	//Loop categories
+	for (car i = 0; i<categories.length; i++) {
+		var html = categoryHtml;
+		var name = "" + categories[i].name;
+		var short_name = categories[i].short_name;
+		html = insertProperty(html, "name", name);
+		html = insertProperty(html, "short_name", short_name);
+		finalHtml += html;
+	}
+	finalHtml += "</section>";
+	return finalHtml;
+}
 
 global.$dc = dc;
 
